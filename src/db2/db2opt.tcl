aebecbbd7445349238590298932e7525e9e0f7b5
Steve Shaw <smshaw@users.sourceforge.net>
2020-06-18 16:51:51 +0100

    multiple labelled connections options for Db2

--- src/db2/db2opt.tcl
+++ src/db2/db2opt.tcl
@@ -120,7 +120,7 @@ upvar #0 configdb2 configdb2
 setlocaltpccvars $configdb2
 #set matching fields in dialog to temporary dict
 variable db2fields
-set db2fields [ dict create connection {db2_def_user {} db2_def_pass {} db2_def_dbase {}} tpcc {db2_user {.tpc.f1.e1 get} db2_pass {.tpc.f1.e2 get} db2_dbase {.tpc.f1.e3 get} db2_def_tab {.tpc.f1.e4 get} db2_tab_list {.tpc.f1.e5 get} db2_total_iterations {.tpc.f1.e14 get} db2_rampup {.tpc.f1.e17 get} db2_duration {.tpc.f1.e18 get} db2_monreport {.tpc.f1.e19 get} db2_async_client {.tpc.f1.e23 get} db2_async_delay {.tpc.f1.e24 get} db2_count_ware $db2_count_ware db2_num_vu $db2_num_vu db2_partition $db2_partition db2_driver $db2_driver db2_raiseerror $db2_raiseerror db2_keyandthink $db2_keyandthink db2_allwarehouse $db2_allwarehouse db2_timeprofile $db2_timeprofile db2_async_scale $db2_async_scale db2_async_verbose $db2_async_verbose} ]
+set db2fields [ dict create connection {db2_def_user {} db2_def_pass {} db2_def_dbase {}} tpcc {db2_user {.tpc.f1.e1 get} db2_pass {.tpc.f1.e2 get} db2_dbase {.tpc.f1.e3 get} db2_def_tab {.tpc.f1.e4 get} db2_tab_list {.tpc.f1.e5 get} db2_total_iterations {.tpc.f1.e14 get} db2_rampup {.tpc.f1.e17 get} db2_duration {.tpc.f1.e18 get} db2_monreport {.tpc.f1.e19 get} db2_async_client {.tpc.f1.e23 get} db2_async_delay {.tpc.f1.e24 get} db2_count_ware $db2_count_ware db2_num_vu $db2_num_vu db2_partition $db2_partition db2_driver $db2_driver db2_raiseerror $db2_raiseerror db2_keyandthink $db2_keyandthink db2_allwarehouse $db2_allwarehouse db2_timeprofile $db2_timeprofile db2_async_scale $db2_async_scale db2_async_verbose $db2_async_verbose db2_connect_pool $db2_connect_pool} ]
 set whlist [ get_warehouse_list_for_spinbox ]
    catch "destroy .tpc"
    ttk::toplevel .tpc
@@ -422,6 +422,12 @@ if {$db2_driver == "test" || $db2_async_scale == "false" } {
         set db2_async_verbose "false"
         $Name configure -state disabled
         }
+   set Name $Parent.f1.e26
+   set Prompt $Parent.f1.p26
+   ttk::label $Prompt -text "XML Connect Pool :"
+ttk::checkbutton $Name -text "" -variable db2_connect_pool -onvalue "true" -offvalue "false"
+   grid $Prompt -column 0 -row 26 -sticky e
+   grid $Name -column 1 -row 26 -sticky ew
 }
 #This is the Cancel button variables stay as before
 set Name $Parent.b2
