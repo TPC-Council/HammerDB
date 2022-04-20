73e443e014a93e83addae94616bd2e39fee718f7
Pooja Jain <99299346+pooja-jain-17@users.noreply.github.com>
2022-04-20 21:41:28 +0530

    Added Dockerfile to build a HammerDB container and TPROC_C sample scripts to build schema and run tests (#339)
    * Added Dockerfile and sample scripts for HammerDB
    
    * Update mysql_tprocc_run.tcl
    
    * Update maria_tprocc_build.tcl
    
    * Update maria_tprocc_run.tcl
    
    * Update mysql_tprocc_build.tcl

---
+++ Docker/sample_scripts/tprocc/maria_tprocc_build.tcl
@@ -0,0 +1,31 @@
+#!/bin/tclsh
+# maintainer: Pooja Jain
+
+#Set the path for logs directory. By Default logs are logged in /tmp
+export TMP="/tmp"
+
+puts "SETTING CONFIGURATION"
+dbset db maria
+dbset bm TPC-C
+
+diset connection maria_host localhost
+diset connection maria_port 3306
+diset connection maria_sock /tmp/mariadb.sock
+
+diset tpcc maria_count_ware 1
+diset tpcc maria_num_vu 1
+diset tpcc maria_pass maria
+diset tpcc maria_dbase tpcc
+diset tpcc maria_storage_engine innodb
+diset tpcc maria_partition false 
+
+print dict
+vuset logtotemp 1
+buildschema
+waittocomplete
+
+#For Advanced configuration changes, check configuration parameters using command "print dict" in ./hammerdbcli
+
+#Command to run the script
+# ./hammerdbcli auto sample_scripts/tprocc/maria_tprocc_build.tcl
+
