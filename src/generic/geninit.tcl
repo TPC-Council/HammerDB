5184bd1717452b07bf26f717401858503396d2d3
Your Name <you@example.com>
2022-06-16 17:41:00 +0800

    Check schema version of xml and sqlite

--- src/generic/geninit.tcl
+++ src/generic/geninit.tcl
@@ -10,9 +10,43 @@ if { [ dict exists $genericdict sqlitedb sqlitedb_dir ] } {
     set sqlitedb_dir ""
 }
 
+#Get xml_schema_version
+if { [ dict exists $genericdict xml_schema version ] } {
+    set xml_schema_version [ dict get $genericdict xml_schema version ]
+} else {
+    set xml_schema_version ""
+}
+
+#Try to get generic config data from SQLite
+set genericdictdb [ SQLite2Dict "generic" ]
+if { $genericdictdb eq "" } {
+    #No SQLite found, save genericdict from XML to SQLite
+    Dict2SQLite "generic" $genericdict
+} else {
+    if { [ dict exists $genericdictdb xml_schema version ] } {
+        set db_schema_version [ dict get $genericdictdb xml_schema version ]
+    } else {
+        set db_schema_version ""
+    }
+
+    #SQLite found, check whether the schema versions from SQLite and XML are consistent
+    if { $xml_schema_version ne $db_schema_version } {
+        puts "The existed SQLite DBs are incompatible with current HammerDB. SQLite DBs will be cleared."
+        foreach { dbname } { generic database db2 mariadb mssqlserver mysql oracle postgresql } {
+            set dbfile [ CheckSQLiteDB $dbname ]
+            #Remove SQLite file, if need to keep old file, use 'file rename'
+            file delete $dbfile
+        }
+        #After remove old SQLite, save genericdict to SQLite DB
+        Dict2SQLite "generic" $genericdict
+    } else {
+        #Use configration from SQLite
+        set genericdict $genericdictdb
+    }
+}
+
 #Load database config from SQLite database.db
 set dbdict [ SQLite2Dict "database" ]
-
 if { $dbdict eq "" } {
     #Load database config from database.xml
     set dbdict [ ::XML::To_Dict config/database.xml ]
@@ -40,14 +74,6 @@ foreach { key } [ dict keys $dbdict ] {
     set $dictname $dbconfdict
     set prefix [ dict get $dbdict $key prefix ]
     lappend dbsrclist "$key/$prefix\opt.tcl" "$key/$prefix\oltp.tcl" "$key/$prefix\olap.tcl" "$key/$prefix\otc.tcl" "$key/$prefix\met.tcl"
-}
-
-#Get generic config data
-set genericdictdb [ SQLite2Dict "generic" ]
-if { $genericdictdb eq "" } {
-    Dict2SQLite "generic" $genericdict
-} else {
-    set genericdict $genericdictdb
 }
 
 #get_xml_data
