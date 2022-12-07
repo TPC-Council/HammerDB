56987ce6fb068b98d0478490ccd1c32c0be61609
root <pooja.jain@intel.com>
2022-12-06 23:58:18 -0800

    Updated scripts for Version 4.6,  Updated TCL & Python Scripts for MariaDB TPROC-C and MariaDB TPROC-H workloads

---
+++ scripts/tcl/maria/tproch/maria_tproch.sh
@@ -0,0 +1,14 @@
+export TMP=`pwd`/TMP
+echo "BUILD HAMMERDB SCHEMA"
+echo "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-"
+./hammerdbcli auto ./scripts/tcl/maria/tproch/maria_tproch_buildschema.tcl 
+echo "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-"
+echo "RUN HAMMERDB TEST"
+echo "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-"
+./hammerdbcli auto ./scripts/tcl/maria/tproch/maria_tproch_run.tcl 
+echo "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-"
+echo "DROP HAMMERDB SCHEMA"
+./hammerdbcli auto ./scripts/tcl/maria/tproch/maria_tproch_deleteschema.tcl
+echo "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-"
+echo "HAMMERDB RESULT"
+./hammerdbcli auto ./scripts/tcl/maria/tproch/maria_tproch_result.tcl 
---
+++ scripts/tcl/maria/tproch/maria_tproch_result.tcl
@@ -0,0 +1,3 @@
+set tmpdir $::env(TMP)
+set ::outputfile $tmpdir/maria_tproch_jobid
+source ./scripts/tcl/generic/generic_tproch_result.tcl 
