a65653caaaac1fe41f18608235fb9d1799cd7150
Steve Shaw <smshaw@users.sourceforge.net>
2023-06-06 13:23:14 +0100

    Fix typo in PostgreSQL TPROC-H scripts

--- scripts/python/postgres/tproch/pg_tproch_py.sh
+++ scripts/python/postgres/tproch/pg_tproch_py.sh
@@ -2,14 +2,14 @@ export TMP=`pwd`/TMP
 mkdir -p $TMP
 echo "BUILD HAMMERDB SCHEMA"
 echo "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-"
-./hammerdbcli py auto ./scripts/python/pg/tproch/pg_tproch_buildschema.py
+./hammerdbcli py auto ./scripts/python/postgres/tproch/pg_tproch_buildschema.py
 echo "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-"
 echo "RUN HAMMERDB TEST"
 echo "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-"
-./hammerdbcli py auto ./scripts/python/pg/tproch/pg_tproch_run.py
+./hammerdbcli py auto ./scripts/python/postgres/tproch/pg_tproch_run.py
 echo "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-"
 echo "DROP HAMMERDB SCHEMA"
-./hammerdbcli py auto ./scripts/python/pg/tproch/pg_tproch_deleteschema.py
+./hammerdbcli py auto ./scripts/python/postgres/tproch/pg_tproch_deleteschema.py
 echo "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-"
 echo "HAMMERDB RESULT"
-./hammerdbcli py auto ./scripts/python/pg/tproch/pg_tproch_result.py
+./hammerdbcli py auto ./scripts/python/postgres/tproch/pg_tproch_result.py
