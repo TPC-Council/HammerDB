TPC-Council/HammerDB

commit 16752d35a50e315b7ad85e5e1506fec1b0839658

Add Schema Existence Check for MySQL/MariaDB (#706)
Merging as approved by TPC-OSS subcommittee on 28th May 2024

src/mysql/mysqlolap.tcl
@@ -159,14 +159,27 @@ proc ConnectToMySQL { host port socket ssl_options user password is_oceanbase ob
     }
 }
 
-proc CreateDatabase { mysql_handler db } {
-    puts "CREATING DATABASE $db"
-    set sql(1) "SET FOREIGN_KEY_CHECKS = 0"
-    set sql(2) "CREATE DATABASE IF NOT EXISTS `$db` CHARACTER SET latin1 COLLATE latin1_swedish_ci"
-    for { set i 1 } { $i <= 2 } { incr i } {
-        mysqlexec $mysql_handler $sql($i)
+proc CreateDatabase { maria_handler db } {
+    puts "CHECKING IF DATABASE $db EXISTS"
+    set db_exists [ maria::sel $maria_handler "SELECT COUNT(*) FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '$db'" -flatlist ]
+    if { $db_exists } {
+        set table_count [ maria::sel $maria_handler "SELECT COUNT(DISTINCT TABLE_NAME) FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = '$db'" -flatlist ]
+        if { $table_count == 0 } {
+            puts "Empty database $db exists"
+            puts "Using existing empty Database $db for Schema build"
+        } else {
+            puts "Database $db exists but is not empty, specify a new or empty database name"
+            return false
+        }
+    } else {
+        puts "CREATING DATABASE $db"
+        set sql(1) "SET FOREIGN_KEY_CHECKS = 0"
+        set sql(2) "CREATE DATABASE IF NOT EXISTS `$db` CHARACTER SET latin1 COLLATE latin1_swedish_ci"
+        for { set i 1 } { $i <= 2 } { incr i } {
+            mariaexec $maria_handler $sql($i)
+        }
     }
-    return
+    return true
 }
 
 proc CreateTables { mysql_handler mysql_tpch_storage_engine } {
@@ -803,12 +816,15 @@ proc do_tpch { host port socket ssl_options scale_fact user password db mysql_tp
             PrepareOceanbase $host $port $socket $ssl_options $user $password $ob_tenant_name
         }
         set mysql_handler [ ConnectToMySQL $host $port $socket $ssl_options $user $password $oceanbase_db $ob_tenant_name]
-    
-        CreateDatabase $mysql_handler $db
+        set db_created [ CreateDatabase $mysql_handler $db ]
+        if { !$db_created } {
+            tsv::set application abort 1
+            error "Database not created"
+        }
         mysqluse $mysql_handler $db
         mysql::autocommit $mysql_handler 0
         # If storage_engine is set to heatwave, first, create the db schema using InnoDB and migrate it after data generation.
-        if {$oceanbase_db == "true" } {
+        if { $oceanbase_db == "true" } {
             CreateOBTables $mysql_handler $ob_partition_num
         } else {
             if { [string equal -nocase $mysql_tpch_storage_engine "Heatwave" ] } {
@@ -817,7 +833,6 @@ proc do_tpch { host port socket ssl_options scale_fact user password db mysql_tp
                 CreateTables $mysql_handler $mysql_tpch_storage_engine
             }
         }
-
         if { $threaded eq "MULTI-THREADED" } {
             tsv::set application load "READY"
             puts "Loading REGION..."
@@ -844,22 +859,25 @@ proc do_tpch { host port socket ssl_options scale_fact user password db mysql_tp
                 set prevactive $lvcnt
                 if { $dncnt eq [expr  $totalvirtualusers - 1] } { break }
                 after 10000
-            }} else {
+            }
+        } else {
             puts "Loading REGION..."
             mk_region $mysql_handler
             puts "Loading REGION COMPLETE"
             puts "Loading NATION..."
             mk_nation $mysql_handler
-            puts "Loading NATION COMPLETE"}}
+            puts "Loading NATION COMPLETE"
+        }
+    }
     if { $threaded eq "SINGLE-THREADED" ||  $threaded eq "MULTI-THREADED" && $myposition != 1 } {
         if { $threaded eq "MULTI-THREADED" } {
             puts "Waiting for Monitor Thread..."
             set mtcnt 0
             while 1 {
+                if { [ tsv::get application abort ] } { return }
                 if { [ tsv::exists application load ] } {
                     incr mtcnt
-                    if {  [ tsv::get application load ] eq "READY" } { break }
-                    if {  [ tsv::get application abort ]  } { return }
+                    if { [ tsv::get application load ] eq "READY" } { break }
                     if { $mtcnt eq 48 } {
                         puts "Monitor failed to notify ready state"
                         return

src/mysql/mysqloltp.tcl
@@ -491,14 +491,27 @@ proc GatherStatistics { mysql_handler } {
     return
 }
 
-proc CreateDatabase { mysql_handler db } {
-    puts "CREATING DATABASE $db"
-    set sql(1) "SET FOREIGN_KEY_CHECKS = 0"
-    set sql(2) "CREATE DATABASE IF NOT EXISTS `$db` CHARACTER SET latin1 COLLATE latin1_swedish_ci"
-    for { set i 1 } { $i <= 2 } { incr i } {
-        mysqlexec $mysql_handler $sql($i)
+proc CreateDatabase { maria_handler db } {
+    puts "CHECKING IF DATABASE $db EXISTS"
+    set db_exists [ maria::sel $maria_handler "SELECT COUNT(*) FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '$db'" -flatlist ]
+    if { $db_exists } {
+        set table_count [ maria::sel $maria_handler "SELECT COUNT(DISTINCT TABLE_NAME) FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = '$db'" -flatlist ]
+        if { $table_count == 0 } {
+            puts "Empty database $db exists"
+            puts "Using existing empty Database $db for Schema build"
+        } else {
+            puts "Database $db exists but is not empty, specify a new or empty database name"
+            return false
+        }
+    } else {
+        puts "CREATING DATABASE $db"
+        set sql(1) "SET FOREIGN_KEY_CHECKS = 0"
+        set sql(2) "CREATE DATABASE IF NOT EXISTS `$db` CHARACTER SET latin1 COLLATE latin1_swedish_ci"
+        for { set i 1 } { $i <= 2 } { incr i } {
+            mariaexec $maria_handler $sql($i)
+        }
     }
-    return
+    return true
 }
 
 proc CreateTables { mysql_handler mysql_storage_engine num_part history_pk } {
@@ -1002,8 +1015,12 @@ proc do_tpcc { host port socket ssl_options count_ware user password db mysql_st
     }
     if { $threaded eq "SINGLE-THREADED" ||  $threaded eq "MULTI-THREADED" && $myposition eq 1 } {
         puts "CREATING [ string toupper $db ] SCHEMA"
-	    set mysql_handler [ ConnectToMySQL $host $port $socket $ssl_options $user $password ]
-        CreateDatabase $mysql_handler $db
+        set mysql_handler [ ConnectToMySQL $host $port $socket $ssl_options $user $password ]
+        set db_created [ CreateDatabase $mysql_handler $db ]
+        if { !$db_created } {
+            tsv::set application abort 1
+            error "Database not created"
+        }
         mysqluse $mysql_handler $db
         mysql::autocommit $mysql_handler 0
         if { $partition eq "true" } {
@@ -1046,9 +1063,10 @@ proc do_tpcc { host port socket ssl_options count_ware user password db mysql_st
             puts "Waiting for Monitor Thread..."
             set mtcnt 0
             while 1 {
+                if { [ tsv::get application abort ] } { return }
                 if { [ tsv::exists application load ] } {
                     incr mtcnt
-                    if {  [ tsv::get application load ] eq "READY" } { break }
+                    if { [ tsv::get application load ] eq "READY" } { break }
                     if { $mtcnt eq 48 } {
                         puts "Monitor failed to notify ready state"
                         return
@@ -1056,7 +1074,7 @@ proc do_tpcc { host port socket ssl_options count_ware user password db mysql_st
                 }
                 after 5000
             }
-	        set mysql_handler [ ConnectToMySQL $host $port $socket $ssl_options $user $password ]
+            set mysql_handler [ ConnectToMySQL $host $port $socket $ssl_options $user $password ]
             mysqluse $mysql_handler $db
             set remb [ lassign [ findchunk $num_vu $count_ware $myposition ] chunk mystart myend ]
             puts "Loading $chunk Warehouses start:$mystart end:$myend"
@@ -1070,7 +1088,7 @@ proc do_tpcc { host port socket ssl_options count_ware user password db mysql_st
         LoadCust $mysql_handler $mystart $myend $CUST_PER_DIST $DIST_PER_WARE
         LoadOrd $mysql_handler $mystart $myend $MAXITEMS $ORD_PER_DIST $DIST_PER_WARE
         puts "End:[ clock format [ clock seconds ] ]"
-        mysql::commit $mysql_handler 
+        mysql::commit $mysql_handler
         if { $threaded eq "MULTI-THREADED" } {
             tsv::lreplace common thrdlst $myposition $myposition done
         }

src/mariadb/mariaolap.tcl
@@ -96,13 +96,26 @@ proc ConnectToMaria { host port socket ssl_options user password } {
 }
 
 proc CreateDatabase { maria_handler db } {
-    puts "CREATING DATABASE $db"
-    set sql(1) "SET FOREIGN_KEY_CHECKS = 0"
-    set sql(2) "CREATE DATABASE IF NOT EXISTS `$db` CHARACTER SET latin1 COLLATE latin1_swedish_ci"
-    for { set i 1 } { $i <= 2 } { incr i } {
-        mariaexec $maria_handler $sql($i)
+    puts "CHECKING IF DATABASE $db EXISTS"
+    set db_exists [ maria::sel $maria_handler "SELECT COUNT(*) FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '$db'" -flatlist ]
+    if { $db_exists } {
+        set table_count [ maria::sel $maria_handler "SELECT COUNT(DISTINCT TABLE_NAME) FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = '$db'" -flatlist ]
+        if { $table_count == 0 } {
+            puts "Empty database $db exists"
+            puts "Using existing empty Database $db for Schema build"
+        } else {
+            puts "Database $db exists but is not empty, specify a new or empty database name"
+            return false
+        }
+    } else {
+        puts "CREATING DATABASE $db"
+        set sql(1) "SET FOREIGN_KEY_CHECKS = 0"
+        set sql(2) "CREATE DATABASE IF NOT EXISTS `$db` CHARACTER SET latin1 COLLATE latin1_swedish_ci"
+        for { set i 1 } { $i <= 2 } { incr i } {
+            mariaexec $maria_handler $sql($i)
+        }
     }
-    return
+    return true
 }
 
 proc GenerateTableQueries { maria_tpch_storage_engine } {
@@ -620,13 +633,17 @@ proc do_tpch { host port socket ssl_options scale_fact user password db maria_tp
         set threaded "SINGLE-THREADED"
         set num_vu 1
     }
-    if { $threaded eq "SINGLE-THREADED" ||  $threaded eq "MULTI-THREADED" && $myposition eq 1 } {
+    if { $threaded eq "SINGLE-THREADED" || $threaded eq "MULTI-THREADED" && $myposition eq 1 } {
         puts "CREATING [ string toupper $user ] SCHEMA"
-	    set maria_handler [ ConnectToMaria $host $port $socket $ssl_options $user $password ]
-            CreateDatabase $maria_handler $db
-            mariause $maria_handler $db
-            maria::autocommit $maria_handler 0
-            CreateTables $maria_handler $maria_tpch_storage_engine
+        set maria_handler [ ConnectToMaria $host $port $socket $ssl_options $user $password ]
+        set db_created [ CreateDatabase $maria_handler $db ]
+        if { !$db_created } {
+            tsv::set application abort 1
+            error "Database not created"
+        }
+        mariause $maria_handler $db
+        maria::autocommit $maria_handler 0
+        CreateTables $maria_handler $maria_tpch_storage_engine
         if { $threaded eq "MULTI-THREADED" } {
             tsv::set application load "READY"
             puts "Loading REGION..."
@@ -663,16 +680,15 @@ proc do_tpch { host port socket ssl_options scale_fact user password db maria_tp
             puts "Loading NATION COMPLETE"
         }
     }
-
     if { $threaded eq "SINGLE-THREADED" ||  $threaded eq "MULTI-THREADED" && $myposition != 1 } {
         if { $threaded eq "MULTI-THREADED" } {
             puts "Waiting for Monitor Thread..."
             set mtcnt 0
             while 1 {
+                if { [ tsv::get application abort ] } { return }
                 if { [ tsv::exists application load ] } {
                     incr mtcnt
-                    if {  [ tsv::get application load ] eq "READY" } { break }
-                    if {  [ tsv::get application abort ]  } { return }
+                    if { [ tsv::get application load ] eq "READY" } { break }
                     if { $mtcnt eq 48 } {
                         puts "Monitor failed to notify ready state"
                         return
@@ -680,9 +696,9 @@ proc do_tpch { host port socket ssl_options scale_fact user password db maria_tp
                 }
                 after 5000
             }
-	    set maria_handler [ ConnectToMaria $host $port $socket $ssl_options $user $password ]
-	    mariause $maria_handler $db
-                mariaexec $maria_handler "SET FOREIGN_KEY_CHECKS = 0"
+            set maria_handler [ ConnectToMaria $host $port $socket $ssl_options $user $password ]
+            mariause $maria_handler $db
+            mariaexec $maria_handler "SET FOREIGN_KEY_CHECKS = 0"
             if { [ expr $myposition - 1 ] > $max_threads } { puts "No Data to Create"; return }
             if { [ expr $num_vu + 1 ] > $max_threads } { set num_vu $max_threads }
             set sf_chunk [ split [ start_end $sup_rows $myposition $sf_mult $num_vu ] ":" ]
@@ -692,8 +708,8 @@ proc do_tpch { host port socket ssl_options scale_fact user password db maria_tp
             tsv::lreplace common thrdlst $myposition $myposition active
         } else {
             set sf_chunk "1 $sup_rows"
-            set cust_chunk "1 [ expr {$sup_rows * $cust_mult} ]" 
-            set part_chunk "1 [ expr {$sup_rows * $part_mult} ]" 
+            set cust_chunk "1 [ expr {$sup_rows * $cust_mult} ]"
+            set part_chunk "1 [ expr {$sup_rows * $part_mult} ]"
             set ord_chunk "1 [ expr {$sup_rows * $ord_mult} ]"
         }
         puts "Start:[ clock format [ clock seconds ] ]"

src/mariadb/mariaoltp.tcl
@@ -511,13 +511,26 @@ proc GatherStatistics { maria_handler } {
 }
 
 proc CreateDatabase { maria_handler db } {
-    puts "CREATING DATABASE $db"
-    set sql(1) "SET FOREIGN_KEY_CHECKS = 0"
-    set sql(2) "CREATE DATABASE IF NOT EXISTS `$db` CHARACTER SET latin1 COLLATE latin1_swedish_ci"
-    for { set i 1 } { $i <= 2 } { incr i } {
-        mariaexec $maria_handler $sql($i)
+    puts "CHECKING IF DATABASE $db EXISTS"
+    set db_exists [ maria::sel $maria_handler "SELECT COUNT(*) FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '$db'" -flatlist ]
+    if { $db_exists } {
+        set table_count [ maria::sel $maria_handler "SELECT COUNT(DISTINCT TABLE_NAME) FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = '$db'" -flatlist ]
+        if { $table_count == 0 } {
+            puts "Empty database $db exists"
+            puts "Using existing empty Database $db for Schema build"
+        } else {
+            puts "Database $db exists but is not empty, specify a new or empty database name"
+            return false
+        }
+    } else {
+        puts "CREATING DATABASE $db"
+        set sql(1) "SET FOREIGN_KEY_CHECKS = 0"
+        set sql(2) "CREATE DATABASE IF NOT EXISTS `$db` CHARACTER SET latin1 COLLATE latin1_swedish_ci"
+        for { set i 1 } { $i <= 2 } { incr i } {
+            mariaexec $maria_handler $sql($i)
+        }
     }
-    return
+    return true
 }
 
 proc CreateTables { maria_handler maria_storage_engine num_part history_pk } {
@@ -1021,20 +1034,24 @@ proc do_tpcc { host port socket ssl_options count_ware user password db maria_st
     }
     if { $threaded eq "SINGLE-THREADED" ||  $threaded eq "MULTI-THREADED" && $myposition eq 1 } {
         puts "CREATING [ string toupper $db ] SCHEMA"
-	set maria_handler [ ConnectToMaria $host $port $socket $ssl_options $user $password ]
-            CreateDatabase $maria_handler $db
-            mariause $maria_handler $db
-            maria::autocommit $maria_handler 0
-            if { $partition eq "true" } {
-                if {$count_ware < 200} {
-                    set num_part 0
-                } else {
-                    set num_part [ expr round($count_ware/100) ]
-                }
-            } else {
+        set maria_handler [ ConnectToMaria $host $port $socket $ssl_options $user $password ]
+        set db_created [ CreateDatabase $maria_handler $db ]
+        if { !$db_created } {
+            tsv::set application abort 1
+            error "Database not created"
+        }
+        mariause $maria_handler $db
+        maria::autocommit $maria_handler 0
+        if { $partition eq "true" } {
+            if {$count_ware < 200} {
                 set num_part 0
+            } else {
+                set num_part [ expr round($count_ware/100) ]
             }
-            CreateTables $maria_handler $maria_storage_engine $num_part $history_pk
+        } else {
+            set num_part 0
+        }
+        CreateTables $maria_handler $maria_storage_engine $num_part $history_pk
         if { $threaded eq "MULTI-THREADED" } {
             tsv::set application load "READY"
             LoadItems $maria_handler $MAXITEMS
@@ -1060,14 +1077,15 @@ proc do_tpcc { host port socket ssl_options count_ware user password db maria_st
             LoadItems $maria_handler $MAXITEMS
         }
     }
-    if { $threaded eq "SINGLE-THREADED" ||  $threaded eq "MULTI-THREADED" && $myposition != 1 } {
+    if { $threaded eq "SINGLE-THREADED" || $threaded eq "MULTI-THREADED" && $myposition != 1 } {
         if { $threaded eq "MULTI-THREADED" } {
             puts "Waiting for Monitor Thread..."
             set mtcnt 0
             while 1 {
+                if { [ tsv::get application abort ] } { return }
                 if { [ tsv::exists application load ] } {
                     incr mtcnt
-                    if {  [ tsv::get application load ] eq "READY" } { break }
+                    if { [ tsv::get application load ] eq "READY" } { break }
                     if { $mtcnt eq 48 } {
                         puts "Monitor failed to notify ready state"
                         return
@@ -1075,7 +1093,7 @@ proc do_tpcc { host port socket ssl_options count_ware user password db maria_st
                 }
                 after 5000
             }
-	set maria_handler [ ConnectToMaria $host $port $socket $ssl_options $user $password ]
+            set maria_handler [ ConnectToMaria $host $port $socket $ssl_options $user $password ]
             mariause $maria_handler $db
             set remb [ lassign [ findchunk $num_vu $count_ware $myposition ] chunk mystart myend ]
             puts "Loading $chunk Warehouses start:$mystart end:$myend"
@@ -1089,7 +1107,7 @@ proc do_tpcc { host port socket ssl_options count_ware user password db maria_st
         LoadCust $maria_handler $mystart $myend $CUST_PER_DIST $DIST_PER_WARE
         LoadOrd $maria_handler $mystart $myend $MAXITEMS $ORD_PER_DIST $DIST_PER_WARE
         puts "End:[ clock format [ clock seconds ] ]"
-        maria::commit $maria_handler 
+        maria::commit $maria_handler
         if { $threaded eq "MULTI-THREADED" } {
             tsv::lreplace common thrdlst $myposition $myposition done
         }

commit a56b984f338a2f8273e88a1eb467218a7542b047

Fix MySQL bug introduced by #706

src/mysql/mysqlolap.tcl
@@ -159,11 +159,11 @@ proc ConnectToMySQL { host port socket ssl_options user password is_oceanbase ob
     }
 }
 
-proc CreateDatabase { maria_handler db } {
+proc CreateDatabase { mysql_handler db } {
     puts "CHECKING IF DATABASE $db EXISTS"
-    set db_exists [ maria::sel $maria_handler "SELECT COUNT(*) FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '$db'" -flatlist ]
+    set db_exists [ mysql::sel $mysql_handler "SELECT COUNT(*) FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '$db'" -flatlist ]
     if { $db_exists } {
-        set table_count [ maria::sel $maria_handler "SELECT COUNT(DISTINCT TABLE_NAME) FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = '$db'" -flatlist ]
+        set table_count [ mysql::sel $mysql_handler "SELECT COUNT(DISTINCT TABLE_NAME) FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = '$db'" -flatlist ]
         if { $table_count == 0 } {
             puts "Empty database $db exists"
             puts "Using existing empty Database $db for Schema build"
@@ -176,7 +176,7 @@ proc CreateDatabase { maria_handler db } {
         set sql(1) "SET FOREIGN_KEY_CHECKS = 0"
         set sql(2) "CREATE DATABASE IF NOT EXISTS `$db` CHARACTER SET latin1 COLLATE latin1_swedish_ci"
         for { set i 1 } { $i <= 2 } { incr i } {
-            mariaexec $maria_handler $sql($i)
+            mysqlexec $mysql_handler $sql($i)
         }
     }
     return true

src/mysql/mysqloltp.tcl
@@ -491,11 +491,11 @@ proc GatherStatistics { mysql_handler } {
     return
 }
 
-proc CreateDatabase { maria_handler db } {
+proc CreateDatabase { mysql_handler db } {
     puts "CHECKING IF DATABASE $db EXISTS"
-    set db_exists [ maria::sel $maria_handler "SELECT COUNT(*) FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '$db'" -flatlist ]
+    set db_exists [ mysql::sel $mysql_handler "SELECT COUNT(*) FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '$db'" -flatlist ]
     if { $db_exists } {
-        set table_count [ maria::sel $maria_handler "SELECT COUNT(DISTINCT TABLE_NAME) FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = '$db'" -flatlist ]
+        set table_count [ mysql::sel $mysql_handler "SELECT COUNT(DISTINCT TABLE_NAME) FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = '$db'" -flatlist ]
         if { $table_count == 0 } {
             puts "Empty database $db exists"
             puts "Using existing empty Database $db for Schema build"
@@ -508,7 +508,7 @@ proc CreateDatabase { maria_handler db } {
         set sql(1) "SET FOREIGN_KEY_CHECKS = 0"
         set sql(2) "CREATE DATABASE IF NOT EXISTS `$db` CHARACTER SET latin1 COLLATE latin1_swedish_ci"
         for { set i 1 } { $i <= 2 } { incr i } {
-            mariaexec $maria_handler $sql($i)
+            mysqlexec $mysql_handler $sql($i)
         }
     }
     return true